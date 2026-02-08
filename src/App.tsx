2340573d655bc3107912fa06eba2d3b64ef2f913
bacowan <cowan.brendan.a@gmail.com>
2026-02-08 18:07:03 +0900

    adding ui library

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-
+import React from "react";
+import { Allotment } from "allotment";
+import "allotment/dist/style.css";
 
 const App = () => {
   return (
-    <main>
+    <main className="h-screen w-screen">
+      <Allotment>
+        <div>ComponentA</div>
+        <div>ComponentB</div>
+      </Allotment>
     </main>
   );
 }
