4ff0efae9b59be3350995f098da99466a46ff01f
bacowan <cowan.brendan.a@gmail.com>
2026-02-10 23:05:01 +0900

    table headers

--- src/components/views/HexSource.tsx
+++ src/components/views/HexSource.tsx
@@ -4,13 +4,13 @@ import { useRef } from "react";
 const emToPx = (em: number) => em * parseFloat(getComputedStyle(document.documentElement).fontSize);
 
 export const HexSource = () => {
-  const headerValues = Array.from({ length: 16 }, (_, i) =>
-    i.toString(16).padStart(2, '0').toUpperCase()
-  );
-
-  const hexValues = Array.from({ length: 256 }, (_, i) =>
-    i.toString(16).padStart(2, '0').toUpperCase()
-  );
+  const headerValues = [
+    "0000",
+    ...
+    Array.from({ length: 16 }, (_, i) =>
+      i.toString(16).padStart(2, '0').toUpperCase()
+    )
+  ];
 
   const parentRef = useRef(null)
 
@@ -23,34 +23,44 @@ export const HexSource = () => {
 
 
   return (
-    <div ref={parentRef} className="h-full overflow-auto">
-      <div
-          className="w-full relative"
-          style={{ height: `${rowVirtualizer.getTotalSize()}px` }}
-        >
-          {rowVirtualizer.getVirtualItems().map((virtualItem) => (
-            <div
-              key={virtualItem.key}
-              className="flex flex-row"
-              style={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                width: '100%',
-                height: `${virtualItem.size}px`,
-                transform: `translateY(${virtualItem.start}px)`,
-              }}>
-              {Array.from({ length: 16 }, (_, i) => {
-                return (
-                  <div
-                    key={i}
-                    className="text-sm font-mono font-bold px-1">
-                      1F
-                  </div>
-                );
-              })}
-            </div>
-          ))}
+    <div className="h-full flex flex-col">
+      <div className="flex flex-row pb-1 border-b border-gray-300">
+        {headerValues.map((hex, index) => (
+          <div key={`header-${hex}`} className={`text-sm font-mono font-bold px-1 ${index === 0 ? 'invisible' : ''}`}>
+            {hex}
+          </div>
+        ))}
+      </div>
+      <div ref={parentRef} className="flex-1 overflow-auto">
+        <div
+            className="w-full relative"
+            style={{ height: `${rowVirtualizer.getTotalSize()}px` }}
+          >
+            {rowVirtualizer.getVirtualItems().map((virtualItem) => (
+              <div
+                key={virtualItem.key}
+                className="flex flex-row"
+                style={{
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  width: '100%',
+                  height: `${virtualItem.size}px`,
+                  transform: `translateY(${virtualItem.start}px)`,
+                }}>
+                  <span className="text-sm font-mono font-bold px-1 border-r border-gray-300">{virtualItem.index.toString(16).padStart(4, '0').toUpperCase()}</span>
+                  {Array.from({ length: 16 }, (_, i) => {
+                    return (
+                      <div
+                        key={i}
+                        className="text-sm font-mono font-bold px-1">
+                          1F
+                      </div>
+                    );
+                  })}
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   )
