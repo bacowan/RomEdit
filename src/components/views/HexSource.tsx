2b13d1723b63a0b3687af6b2f65935622e1546d1
bacowan <cowan.brendan.a@gmail.com>
2026-02-10 22:52:51 +0900

    virtualized hex grid (no loading)

--- src/components/views/HexSource.tsx
+++ src/components/views/HexSource.tsx
@@ -1,3 +1,8 @@
+import { useVirtualizer } from "@tanstack/react-virtual";
+import { useRef } from "react";
+
+const emToPx = (em: number) => em * parseFloat(getComputedStyle(document.documentElement).fontSize);
+
 export const HexSource = () => {
   const headerValues = Array.from({ length: 16 }, (_, i) =>
     i.toString(16).padStart(2, '0').toUpperCase()
@@ -7,31 +12,46 @@ export const HexSource = () => {
     i.toString(16).padStart(2, '0').toUpperCase()
   );
 
+  const parentRef = useRef(null)
+
+  // The virtualizer
+  const rowVirtualizer = useVirtualizer({
+    count: 10000,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => emToPx(1.2),
+  })
+
+
   return (
-    <div className="p-4 overflow-x-auto h-full">
-      <div className="grid w-fit grid-cols-[3rem_repeat(16,1.5rem)]">
-        <div className="text-sm font-mono font-bold pb-1 border-b border-gray-300" />
-        {headerValues.map((hex) => (
-          <div key={`header-${hex}`} className="text-sm font-mono font-bold pb-1 border-b border-gray-300">
-            {hex}
-          </div>
-        ))}
-        {hexValues.map((hex, index) => (
-          <>
-            {index % 16 === 0 && (
-              <div key={`offset-${index}`} className="text-sm font-mono font-bold pr-2 border-r border-gray-300">
-                {(index).toString(16).padStart(4, '0').toUpperCase()}
-              </div>
-            )}
+    <div ref={parentRef} className="h-full overflow-auto">
+      <div
+          className="w-full relative"
+          style={{ height: `${rowVirtualizer.getTotalSize()}px` }}
+        >
+          {rowVirtualizer.getVirtualItems().map((virtualItem) => (
             <div
-              key={index}
-              className="text-sm font-mono"
-            >
-              {hex}
+              key={virtualItem.key}
+              className="flex flex-row"
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: `${virtualItem.size}px`,
+                transform: `translateY(${virtualItem.start}px)`,
+              }}>
+              {Array.from({ length: 16 }, (_, i) => {
+                return (
+                  <div
+                    key={i}
+                    className="text-sm font-mono font-bold px-1">
+                      1F
+                  </div>
+                );
+              })}
             </div>
-          </>
-        ))}
+          ))}
       </div>
     </div>
-  );
+  )
 }
